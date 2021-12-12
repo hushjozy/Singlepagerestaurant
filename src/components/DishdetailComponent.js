88f53045de904afd6d429aed2f20a78bcade8af9
hushjozy <67883115+hushjozy@users.noreply.github.com>
2021-12-12 03:21:31 +0100

    Initial commit

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,79 @@
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+
+function RenderDish({ dish }) {
+  if (dish != null)
+    return (
+      <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  else return <div></div>;
+}
+
+function RenderComments({ comments }) {
+  if (comments != null) {
+    return (
+      <div className="col-12 col-5 m-1">
+        <h4>Comments</h4>
+        <ul className="list-unstyled">
+          <li key={comments.id}>
+            <p>{comments.comment}</p>
+            <p>
+              -- {comments.author} ,{" "}
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+              }).format(new Date(Date.parse(comments.date)))}
+            </p>
+          </li>
+          );
+        </ul>
+      </div>
+    );
+  }
+}
+
+const Dishdetail = (props) => {
+  if (props.dish != null) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6 col-md-12 m-1">
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className="col col-md-12 m-1">
+            <RenderComments comments={props.comments} />
+          </div>
+        </div>
+      </div>
+    );
+  } else return <div></div>;
+};
+export default Dishdetail;
