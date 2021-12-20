b8ae4065e6ca1fcbf9a1663c3b62220b891ac92f
hushjozy <67883115+hushjozy@users.noreply.github.com>
2021-12-20 13:47:01 +0100

    React Animations

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,11 +10,16 @@ import About from "./AboutComponent";
 import { COMMENTS } from "../shared/comments";
 import { LEADERS } from "../shared/leaders";
 import { PROMOTIONS } from "../shared/promotions";
-import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
+import {
+  postComment,
+  fetchDishes,
+  fetchComments,
+  fetchPromos
+} from "../redux/ActionCreators";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { actions } from "react-redux-form";
-
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const mapStateToProps = (state) => {
   return {
@@ -25,14 +30,18 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = dispatch => ({
-  fetchDishes: () => { dispatch(fetchDishes())},
-  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
+const mapDispatchToProps = (dispatch) => ({
+  fetchDishes: () => {
+    dispatch(fetchDishes());
+  },
+  resetFeedbackForm: () => {
+    dispatch(actions.reset("feedback"));
+  },
   fetchComments: () => dispatch(fetchComments()),
   fetchPromos: () => dispatch(fetchPromos()),
-  postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment))
+  postComment: (dishId, rating, author, comment) =>
+    dispatch(postComment(dishId, rating, author, comment))
 });
-
 
 class Main extends Component {
   constructor(props) {
@@ -43,7 +52,7 @@ class Main extends Component {
       comments: COMMENTS,
       promotions: PROMOTIONS,
       leaders: LEADERS
-    }
+    };
   }
 
   componentDidMount() {
@@ -51,62 +60,79 @@ class Main extends Component {
     this.props.fetchComments();
     this.props.fetchPromos();
   }
-  
+
   render() {
     const HomePage = () => {
       return (
-        <Home 
-        dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
-        dishesLoading={this.props.dishes.isLoading}
-        dishErrMess={this.props.dishes.errMess}
-        promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
-        promoLoading={this.props.promotions.isLoading}
-        promoErrMess={this.props.promotions.errMess}
-        leader={this.props.leaders.filter((leader) => leader.featured)[0]}
-    />
-    
+        <Home
+          dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+          dishesLoading={this.props.dishes.isLoading}
+          dishErrMess={this.props.dishes.errMess}
+          promotion={
+            this.props.promotions.promotions.filter(
+              (promo) => promo.featured
+            )[0]
+          }
+          promoLoading={this.props.promotions.isLoading}
+          promoErrMess={this.props.promotions.errMess}
+          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     };
 
     const DishWithId = ({ match }) => {
       return (
-        <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-        isLoading={this.props.dishes.isLoading}
-        errMess={this.props.dishes.errMess}
-        comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-        commentsErrMess={this.props.comments.errMess}
-        postComment={this.props.postComment}
-      />
-      
+        <DishDetail
+          dish={
+            this.props.dishes.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
+          comments={this.props.comments.comments.filter(
+            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+          commentsErrMess={this.props.comments.errMess}
+          postComment={this.props.postComment}
+        />
       );
     };
     return (
       <div>
         <Header />
         <div>
-          <Switch>
-            <Route path="/home" component={HomePage} />
-            <Route
-              exact
-              path="/aboutus"
-              component={() => <About leaders={this.props.leaders} />}
-            />
-            } />
-            <Route
-              exact
-              path="/menu"
-              component={() => <Menu dishes={this.props.dishes} />}
-            />
-            <Route path="/menu/:dishId" component={DishWithId} />
-            <Route
-              exact
-              path="/contactus"
-              component={() => (
-                <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
-              )}
-            />
-            <Redirect to="/home" />
-          </Switch>
+          <TransitionGroup>
+            <CSSTransition
+              key={this.props.location.key}
+              classNames="page"
+              timeout={300}
+            >
+              <Switch location={this.props.location}>
+                <Route path="/home" component={HomePage} />
+                <Route
+                  exact
+                  path="/aboutus"
+                  component={() => <About leaders={this.props.leaders} />}
+                />
+                } />
+                <Route
+                  exact
+                  path="/menu"
+                  component={() => <Menu dishes={this.props.dishes} />}
+                />
+                <Route path="/menu/:dishId" component={DishWithId} />
+                <Route
+                  exact
+                  path="/contactus"
+                  component={() => (
+                    <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
+                  )}
+                />
+                <Redirect to="/home" />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
         </div>
         <Footer />
       </div>
