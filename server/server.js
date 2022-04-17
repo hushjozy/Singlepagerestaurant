7c607ae9f48537314cd17c786be9b6a715329205
hushjozy <67883115+hushjozy@users.noreply.github.com>
2022-04-17 17:14:56 +0100

    added server server with express

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require("express");
+const nodemailer =require ("nodemailer");
+const request =require ("request");
+const bodyParser = require("body-parser");
+const path = require('path');
+
+const app= express();
+const port = 3000
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+app.post("/",(req,res) =>{
+
+})
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
